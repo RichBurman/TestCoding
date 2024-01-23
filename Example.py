e49a09c846fe2a8e6752c8c8cba659de245f944d
RichBurman <rich226402@googlemail.com>
2024-01-23 09:24:23 +0000

    Add superclasses

--- Example.py
+++ Example.py
@@ -1872,34 +1872,213 @@
 
 # Example 3
 
-class TicketMixin:
-    """
-    Mixin to calculate ticket price based on age
-    """
-    def calculate_ticket_price(self, age):
-        if age < 12: # if age is less than 12
-            price = 0
-        elif age < 18: # if age is less than 18
-            price = 15
-        elif age < 60: # if age is less than 60
-            price = 20
-        else:
-            price = 10 # if age is greater than 60
-        return price
+# class TicketMixin:
+#     """
+#     Mixin to calculate ticket price based on age
+#     """
+#     def calculate_ticket_price(self, age):
+#         if age < 12: # if age is less than 12
+#             price = 0
+#         elif age < 18: # if age is less than 18
+#             price = 15
+#         elif age < 60: # if age is less than 60
+#             price = 20
+#         else:
+#             price = 10 # if age is greater than 60
+#         return price
 
-class Customer(TicketMixin): # Customer inherits from TicketMixin
+# class Customer(TicketMixin): # Customer inherits from TicketMixin
+#     """
+#     Create instance of Customer
+#     """
+#     def __init__(self, name, age): # Customer has a name and age
+#         self.name = name
+#         self.age = age
+        
+#     def describe(self):
+#         return f"{self.name} age {self.age} ticket price is {self.calculate_ticket_price(self.age)}" # uses the mixin method
+        
+# customer = Customer("Ryan Phillips", 22) # creates an instance of Customer
+# print(customer.describe()) # prints the result of the describe method
+
+# Superclass
+
+# Example 1
+
+# class Rectangle:
+#     def __init__(self, height, width):
+#         self.height = height
+#         self.width = width
+
+#     def area(self):
+#         return self.height * self.width
+
+#     def perimeter(self):
+#         return 2 * (self.height + self.width)
+
+
+# class Square(Rectangle):
+#     def __init__(self, height):
+#         super().__init__(height, height)
+
+
+# Example 2
+
+# class Bird(object):
+#     """
+#     Bird superclass
+#     """
+#     def __init__(self, kind, call):
+#         # instance attributes
+#         self.kind = kind
+#         self.call = call
+
+#     def description(self):
+#         """
+#         Returns description string including instance attributes
+#         """
+#         return f'A {self.kind} goes {self.call}'
+
+
+# class Fowl(Bird):
+#     """
+#     Subclass of the superclass Bird
+#     """
+#     def __init__(self, kind, call, type):
+#         self.fowl_types = {'landfowl': 'Landfowl is an order of heavy-bodied ground-feeding birds that includes\n'
+#                                        'turkey, grouse, chicken, New World quail and Old World quail,\n'
+#                                        'ptarmigan, partridge, pheasant, junglefowl and the Cracidae\n',
+#                            'waterfowl': 'Waterfowl is an order of birds that comprises about 180 living species\n'
+#                                         'in three families: Anhimidae (the screamers), Anseranatidae\n'
+#                                         '(the magpie goose), and Anatidae,the largest family, which\n'
+#                                         'includes over 170 species of waterfowl,\n'
+#                                         'among them the ducks, geese, and swans.\n'}
+#         self.type = type
+#         # Uses super() function to state kind, call from superclass Bird
+#         super().__init__(kind, call)
+
+
+#     def description(self):
+#         """
+#         Returns string from superclass description method and
+#         appends a string to include additional information
+#         """
+#         return f'{super().description()} \nSome interesting facts about the {self.kind} : A {self.kind} is of type {self.type}. {self.fowl_types[self.type.lower()]}'
+
+# class Seabird(Bird):
+#     """
+#     Subclass of Bird superclass for sea birds
+#     """
+#     def __init__(self, kind, call, diving_depth):
+#         # super() is used to denote what is inherited from Bird
+#         super().__init__(kind, call)
+#         self.diving_depth = diving_depth
+
+#     def get_description(self):
+#         """
+#         Returns description from superclass using super() function
+#         Appends additional information as a string
+#         """
+#         return f'{super().description()} and also, a {self.kind} dives to a depth of {self.diving_depth} metres.'
+
+
+# gannet = Seabird('Gannet', 'Squawk', 3)
+# print(gannet.get_description())
+
+# duck = Fowl('Mallard', 'Quack!', 'Waterfowl')
+# print(duck.description())
+
+# # Example 3
+
+# class BaseOrderItem():
+#     """
+#     Creates instance of BaseOrderItem
+#     """
+#     def __init__(self, product, price, quantity):
+#         self.product = product
+#         self.price = price
+#         self.quantity = quantity
+    
+#     """
+#     Returns total based on price * quantity ordered
+#     """
+#     def total_price(self):
+#         return self.price * self.quantity
+
+# # write your code here
+
+# class ShoeOrderItem(BaseOrderItem):
+#     """
+#     Create instance of ShoeOrderItem
+#     """
+#     def __init__(self, product, price, quantity, size):
+#         super().__init__(product, price, quantity)
+#         self.size = size
+    
+#     def describe(self):
+#         return f"Order: {self.product} in size {self.size}, quantity: {self.quantity}, total price {self.total_price()}"
+        
+# shoe_order = ShoeOrderItem("Ladies Nike Trainers", 40, 2, 42)
+
+# print(shoe_order.describe())
+
+# Passing Methods Around
+
+# class Bird:
+#     """
+#     Bird class
+#     """
+#     # class attribute
+#     definition = "a warm-blooded egg-laying vertebrate animal distinguished by the possession of feathers, wings, a beak, and typically by being able to fly."
+
+#     def __init__(self, kind, call):
+#         # instance attribute
+#         self.kind = kind
+#         self.call = call
+
+#     def description(self):
+#         """
+#         describe the bird
+#         """
+#         return f"A {self.kind} goes {self.call} and is {self.definition}" 
+
+
+# class Parrot(Bird):
+#     def __init__(self, color):
+#         Bird.__init__(self, 'Parrot', 'Kah!')
+#         self.color = color
+
+
+# parrot = Parrot('blue')
+# print(parrot.color)
+# print(parrot.description())
+
+
+# Example 4
+
+class Employee: # Employee is the superclass
     """
-    Create instance of Customer
+    Creates an instance of Employee
     """
-    def __init__(self, name, age): # Customer has a name and age
+    def __init__(self, name, annual_salary): # Employee has a name and annual salary
         self.name = name
-        self.age = age
-        
-    def describe(self):
-        return f"{self.name} age {self.age} ticket price is {self.calculate_ticket_price(self.age)}" # uses the mixin method
-        
-customer = Customer("Ryan Phillips", 22) # creates an instance of Customer
-print(customer.describe()) # prints the result of the describe method
+        self.annual_salary = annual_salary
+    
+    def calculate_monthly_salary(self): # Employee has a method to calculate monthly salary
+        return self.annual_salary / 12
+
+class CustomerServiceEmployee(Employee): # CustomerServiceEmployee is a subclass of Employee
+    """
+    Creates an instance of CustomerServiceEmployee
+    """
+    def __init__(self, name, annual_salary, department): # CustomerServiceEmployee has a name, annual salary and department
+        super().__init__(name, annual_salary) # super() is used to denote what is inherited from Employee
+        self.department = department
+
+cs_manager = CustomerServiceEmployee("Kelly Johnson", 42000, "Customer Service") # creates an instance of CustomerServiceEmployee
+kellys_monthly_salary = cs_manager.calculate_monthly_salary() # uses the calculate_monthly_salary method from Employee
+print(kellys_monthly_salary) 
+
 
 
 

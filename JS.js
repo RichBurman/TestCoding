29f00aa62340d005e5593d1ceb6a00870fa41b74
RichBurman <rich226402@googlemail.com>
2024-02-06 10:56:53 +0000

    Create JS.js, additional practise on JavaScript

---
+++ JS.js
@@ -0,0 +1,21 @@
+// Practise of JavaScript Coding
+// Reminder to run JS code in the terminal: node JS.js
+
+// let myVar = 5;
+// console.log(myVar);
+
+// const pi = 3.14;
+// let radius = 12;
+// var circumference = pi*2*radius;
+
+
+// console.log(pi);
+// console.log(radius);
+// console.log(circumference);
+
+let globalVar = "My Global Variable";
+
+for (let i =0; i < 5; i++){
+    let localVar = "My Local Variable";
+    console.log(i);
+}
